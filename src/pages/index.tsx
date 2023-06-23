b17c127658f6d16562202931507c04ee37493d19
Tomas Aranda <tomasaranda2018@gmail.com>
2023-06-23 14:08:22 -0300

    add fetch to firebase data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,97 +1,172 @@
-import { ReactElement, useEffect, useState } from "react"
-import { MainLayout } from "@/layouts/MainLayout";
-import Image from "next/image"
-import Link from "next/link"
-import { IconArrowNarrowRight, IconArrowsShuffle } from "@tabler/icons-react";
-import { ButtonCustom } from "@/components/Buttons";
-import { CardMaterias } from "@/components/CardMaterias";
-import { useRouter } from "next/router";
-// import { materias } from "@/utils/json";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "@/utils/firebase";
-import { Skeleton } from "@mantine/core";
-import { useWidth } from "@/utils/helpers";
+import Head from 'next/head'
+import Image from 'next/image'
+// import Loading from '../components/Loading'
+import { headerText } from '../utils/text'
+import { SkeletonFeedbackIndexMobile, SkeletonFeedbackIndexDesktop } from '../components/SkeletonFeedback'
+import { MainLayout } from '@/layouts/MainLayout'
+import { ReactElement, useEffect, useState } from 'react'
+import { IconBrandGithub, IconBrandLinkedin } from '@tabler/icons-react'
+import { TitleSection } from '@/components/TitleSection'
+import { getExperiences, getInfoMe } from '@/services/getDataFirebase'
+import { CardWorkExperience } from '@/components/CardWorkExperience'
 
-export interface Materias {
-  name: string;
-  image: string
-}
-export default function Home() {
-  const router = useRouter()
-  const [materias, setMaterias] = useState<Materias[]>()
-  const [loading, setLoading] = useState<boolean>()
-  const width = useWidth()
+
+function Home() {
+  const [dataExperiences, setDataExperiences] = useState([])
+  const [dataMe, setDataMe] = useState([])
+  const [loading, serLoading] = useState(true)
   
+
   useEffect(() => {
-    const getDataQuiz = async () => {
-      setLoading(true)
-      let arrDocs : Materias[] = []
-      try {
-        const querySnapshot = await getDocs(collection(db, 'materias'));
-        querySnapshot.forEach((doc) => {
-          arrDocs.push(doc.data() as Materias)
-        });
-        setMaterias(arrDocs)
-      } catch (error) {
-          console.log(error)
-      }
-      setLoading(false)
+    const fetchExperiences = async () => {
+      serLoading(true)
+      const data = await getExperiences()
+      setDataExperiences(data)
     }
-      getDataQuiz()
-   }, [])
+
+    const fetchInfoMe = async () => {
+      serLoading(true)
+      const data = await getInfoMe()
+      setDataMe(data)
+      serLoading(false)
+    }
+
+    fetchExperiences()
+    fetchInfoMe()
+  }, [])
+
+  if(loading) return <div>
+    <div className='hidden lg:block'>
+      <SkeletonFeedbackIndexDesktop/>
+    </div>
+    <div className='lg:hidden'>
+      <SkeletonFeedbackIndexMobile/>
+    </div>
+  </div>
+
+
+  const socialMedia = [
+    {
+      href: dataMe.links.linkedin,
+      name: 'LinkedIn',
+      icon: <IconBrandLinkedin/>
+    },
+    // {
+    //   href: dataMe.links.instagram,
+    //   name: 'Instagram',
+    //   icon: instagramIcon
+    // },
+    {
+      href: dataMe.links.github,
+      name: 'GitHub',
+      icon: <IconBrandGithub/>
+    },
+  ]
 
   return (
-    <div className="">
-          <div className="flex justify-around  gap-20 my-5 lg:my-10   ">
-            <div className="">
-              <div className="my-3 lg:mt-10">
-                <h1 className="text-xl text-neutral-700 font-medium mb-4">¡Bienvenido a MedDesafio!</h1>
-                <h2 className="text-4xl xl:text-6xl font-bold mb-4">Prepárate para tu ingreso a Medicina.</h2>
-                <p className="text-neutral-700">Supera el ingreso a Medicina con éxito: Tu camino hacia una carrera de excelencia médica comienza aquí, con MedDesafio.</p>
-              </div>
-              <div className="flex items-center gap-3 mt-10 lg:mt-20 sm:w-1/2 lg:w-[80%] xl:w-2/3 2xl:w-1/2">
-               <ButtonCustom icon={<IconArrowNarrowRight/>} text="Comenzar ahora" onClick={() => router.push(`/quiz/quizAlAzar`)} />
-              </div>
-            </div>
 
-          <div className="hidden lg:block">
-            <Image src={'/banner_home.png'} height={600} width={1000} alt="banner_home" 
-            className=" object-container z-0"/>
+    <div>
+      <header className=''>
+        <div className='flex flex-col-reverse md:flex-row md:justify-between mt-10'>
+          <div className='md:w-[65%] lg:-[73%] items-start flex flex-col gap-4'>
+              <h1 className='font-bold text-4xl'>👋🏼{" "}{headerText.title}{' '}{dataMe.name}.</h1>
+              <h2 className='text-gray-50 dark:text-gray-800 text-opacity-80 font-medium'>I am working as {dataMe.position}{" "}in{" "}
+              <a href={dataMe.webWork} rel="noreferrer" target="_blank" className='text-gray-50 duration-200
+              hover:text-opacity-75 dark:text-gray-800 text-opacity-60'>{dataMe.work}</a>
+              </h2>
+              <h3 className='text-base text-gray-100 dark:text-dark-800 text-opacity-80'>{dataMe?.description?.english}</h3>
           </div>
-          </div>
-
-          <div className="mt-32">
-            <h3 className="text-2xl font-semibold sm:text-center">Desafía tus conocimientos en las cuatro materias claves.</h3>
-            
-            <div className="grid xs:justify-center xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 mt-4 items-center w-full">
-                { !loading ?
-                  materias?.map(item => (
-                    item.name !== 'quizAlAzar' && <Link href={`/quiz/${item.name}`} key={`${item.name}012adasd`}>
-                      <CardMaterias item={item}/>
-                    </Link>
-                  ))
-                :
-                [0, 1, 3, 4].map((_, idx) => (
-                  <div key={`${idx}001334`} className="flex justify-center">
-                    <Skeleton visible={loading} radius={8} height={ 260} width={'100%'}></Skeleton>
-                  </div>
-                  ))
-                }
+          <div className='flex md:flex-col md:justify-center md:items-start sm:items-center gap-3 mb-7'>
+            <Image alt="image" src={dataMe.img} height={190} width={190}/> 
+            <div className='md:flex hidden items-end md:items-start mx-auto w-1/2 flex-col gap-2 mt-3 md:mt-1 '>
+              {
+                socialMedia.map(social => (
+                    <a href={social.href}  target="_blank" rel="noreferrer" className="flex items-center  
+                    hover:scale-105 duration-200 
+                    gap-2 text-white dark:text-dark-800 text-xl
+                    sm:text-base text-opacity-60 hover:text-white" key={social.name}>
+                        {social.icon}
+                        {social.name}
+                    </a>
+                ))
+              }
             </div>
           </div>
+        </div>
 
-          <div className="mt-20 flex flex-col gap-4 justify-center items-center">
-            <p className="text-lg w-full md:w-3/4 xl:w-1/2 font-medium text-left md:text-center">O también puedes hacer un cuestionario con preguntas al azar de las cuatro materias.</p>
-            <ButtonCustom 
-            icon={<IconArrowsShuffle/>}
-            textSize="lg" width={'w-full md:w-3/4 xl:w-1/2'} text="Preguntas al azar" 
-            onClick={() => router.push(`/quiz/quizAlAzar`)} typeButton = {3}/>
+        <div className='flex  md:hidden md:flex-col gap-4  w-[90%] md:my-0 my-5'>
+            {
+              socialMedia.map(social => (
+                  <a href={social.href} target="_blank" rel="noreferrer" className="flex items-center 
+                  hover:scale-105 duration-200 
+                  gap-2 text-white dark:text-dark-800 text-base text-opacity-60 hover:text-white" key={social.name}>
+                      {social.icon}
+                      {social.name}
+                  </a>
+              ))
+            }
           </div>
+
+        <div className='mt-10 md:mt-[0px]'>
+          <p className='mb-3 text-gray-50 dark:text-dark-800 text-opacity-80'>My favorite tech</p>
+          <div className='flex gap-3 items-center md:w-[70%]'>
+            <div className='flex flex-wrap gap-5'>
+              {headerText.tecn.map((tecn) => (
+                  <div className='flex items-center gap-1' key={tecn.name}>
+                    {!tecn.svg ? <Image alt="image" src={`/icons/${tecn.icon}.svg`} height={18} width={18}/>: tecn.icon}
+                    <p className='text-xs text-white dark:text-dark-900 text-opacity-50'>{tecn.name}</p>
+                  </div>  
+              ))}
+            </div>
+          </div>
+        </div>
+      </header>
+
+      {/* Section work Experience */}
+      <section className='mt-14'>
+        <TitleSection title="Work Experience" subtitle="My work experience so far."/>
+
+
+        <div className='flex flex-col gap-5 mt-10' >
+        {dataExperiences.map(work => (
+          <div key={work.company + work.position}>
+            <CardWorkExperience data={work}/>
+          </div>
+        ))}
+        
+        </div>
+        
+    </section>
+      
+    {/* <section className='mt-14'>
+        <TitleSection title="Projects" 
+        subtitle={"I created a few projects to learn more about the technologies I use. You can check them out here. Let me know what you think!"}/>
+
+        <div className='flex flex-col gap-4 mt-10 '>
+          {currentProjects.map((data, index) => (
+              <ProjectsCardsIndex projects={data} key={data.id}/>
+          ))}
+
+        </div>
+
+        <Link href={'/projects'}>
+          <button type='button' 
+          className='flex gap-1 text-white dark:text-dark-900 hover:translate-x-1 duration-300 hover:duration-200
+          text-opacity-70 text-[13px] items-center mt-5'>
+            See all projects
+            <FiArrowRight />
+          </button>
+        </Link>
+
+    </section> */}
+
+
     </div>
-  );
+  )
 }
 
 
+export default Home
 
 Home.getLayout = function getLayout(page: ReactElement) {
   return (
