b986c20bdb3067b63cf3059701ac7e9da4e26053
Tomas Aranda <tomasaranda2018@gmail.com>
2023-06-23 14:05:04 -0300

    add text for tec

---
+++ src/utils/text.ts
@@ -0,0 +1,43 @@
+import { Ic_next } from "@/assets/Ic_next";
+
+export const headerText = {
+    tecn: [
+        {
+            icon: "javascript",
+            name: "JavaScript"
+        },
+        {
+            icon: "typescript",
+            name: "TypeScript"
+        },
+        {
+            icon: "reactjs",
+            name: "React js"
+        },
+        {
+            icon: Ic_next,
+            name: "Next js",
+            svg: true
+        },
+        {
+            icon: "react native",
+            name: "React Native"
+        },
+        {
+            icon: "tailwind",
+            name: "Tailwind"
+        },
+        {
+            icon: "nodejs",
+            name: "Node js"
+        },
+        {
+            icon: "mongodb",
+            name: "MongoDB"
+        },
+        {
+            icon: "firebase",
+            name: "Firebase"
+        },
+    ]
+}
