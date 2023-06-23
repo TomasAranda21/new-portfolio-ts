16572c050da65da7537a2f959bef7e60e11dd608
Tomas Aranda <tomasaranda2018@gmail.com>
2023-06-23 14:07:37 -0300

    change styles and add dark mode

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,22 +1,36 @@
 import { FooterMain } from '@/components/FooterMain'
-import { Header } from '@/components/Header'
+import { HeaderMain, HeaderResponsive } from '@/components/Header'
+import { useWidth } from '@/hook/useWidth'
 import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
 
 export interface childrenProps {
-    children: JSX.Element | JSX.Element[]
-    heightScreen?: boolean 
+    children:  ReactNode
  }
 
-export const MainLayout = ({children, heightScreen=false}: childrenProps) => {
+export const MainLayout : React.FC<childrenProps> = ({children}) => {
+  const width = useWidth()
+
+  useEffect(() => {
+    if (localStorage.theme === "light") {
+        document.documentElement.classList.remove('dark')
+    } else {
+        document.documentElement.classList.add('dark')
+    }
+  }, [])
+
   return (
-    <div className='w-full'>
-        <Header/>
-        <div className={`text-black h-full pt-10 
-        w-full p-3 md:p-6 lg:px-0 lg:w-[70%] mx-auto relative`}>
-            {children}
+    <div className='bg-dark-800 dark:bg-gray-50'>
+      <div className="md:w-[95%] lg:w-[70%] xl:w-[55%] 
+      2xl:w-[43%] 3xl:w-[40%] px-4 sm:px-5 lg:px-0 mx-auto pt-10 text-white dark:text-dark-900">
+        {width > 650 ? <HeaderMain/> :
+        <HeaderResponsive/>}
+        
+        <div className='mt-14'>
+          {children}
         </div>
-        <FooterMain/> 
+        <FooterMain/>
+        </div>
     </div>
   )
 }
